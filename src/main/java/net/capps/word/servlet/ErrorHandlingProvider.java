0afd40dbc2a376832b025755c8ad3f92abb69481
Charles Capps <charles.capps@jivesoftware.com>
2015-03-30 21:12:53 -0700

    More error handling

--- src/main/java/net/capps/word/servlet/ErrorHandlingProvider.java
+++ src/main/java/net/capps/word/servlet/ErrorHandlingProvider.java
@@ -2,6 +2,8 @@ package net.capps.word.servlet;
 
 import com.google.common.base.Throwables;
 import net.capps.word.rest.models.ErrorModel;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -9,12 +11,13 @@ import javax.ws.rs.ext.ExceptionMapper;
 import javax.ws.rs.ext.Provider;
 
 @Provider
- public class ErrorHandlingProvider implements ExceptionMapper<Throwable>
-{
+ public class ErrorHandlingProvider implements ExceptionMapper<Throwable> {
+    private static final Logger LOG = LoggerFactory.getLogger(ErrorHandlingProvider.class);
 
     @Override
     public Response toResponse(Throwable exception)
     {
+        LOG.error("ERROR: {}", Throwables.getStackTraceAsString(exception));
         return Response
                 .status(Response.Status.BAD_REQUEST)
                 .entity(new ErrorModel(Throwables.getStackTraceAsString(exception)))
