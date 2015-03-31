22c4b10be9ee540d002b65a66122e9daf8fc98f2
Charles Capps <charles.capps@jivesoftware.com>
2015-03-30 23:02:08 -0700

    Stupid logging

--- src/main/java/net/capps/word/rest/services/MovesService.java
+++ src/main/java/net/capps/word/rest/services/MovesService.java
@@ -46,24 +46,31 @@ public class MovesService {
         try {
             LOG.info("MOVES SERVICE: PLAY MOVE. PLAY MOVE. FOO FOO FOO");
             UserModel authUser = (UserModel) request.getAttribute(AuthHelper.AUTH_USER_PROPERTY);
+            LOG.info("MOVES SERVICE: got auth user");
             if (authUser == null) {
+                LOG.info("MOVES SERVICE: auth user is null");
                 return Response.status(Status.UNAUTHORIZED)
                         .entity(new ErrorModel("You must login to send a move."))
                         .build();
             }
+            LOG.info("MOVES SERVICE: about to validate move...");
             ErrorOrResult<GameModel> errorOrResult = movesProvider.validateMove(input, authUser);
+            LOG.info("MOVES SERVICE: validated move!");
 
             Optional<ErrorModel> errorOpt = errorOrResult.getError();
             if (errorOpt.isPresent()) {
+                LOG.info("MOVES SERVICE: Error Opt is present: {}", errorOpt.get());
                 return Response.status(Status.BAD_REQUEST)
                         .entity(errorOpt.get())
                         .build();
             }
+            LOG.info("MOVES SERVICE: Move validation success");
 
             final GameModel originalGame = errorOrResult.getResult().get();
 
             // We need to rollback if a failure occurs, so use a common database connection with autoCommit == false
             try (Connection dbConn = WordDbManager.getInstance().getConnection()) {
+                LOG.info("MOVES SERVICE: setting autocommit to false");
                 dbConn.setAutoCommit(false);
 
                 GameModel updatedGame = movesProvider.playMove(input, originalGame, dbConn);
@@ -72,16 +79,20 @@ public class MovesService {
 
                 // For single player games, play the AI's move if the turn changed
                 if (updatedGame.getGameType() == GameType.SINGLE_PLAYER && isAiTurn) {
+                    LOG.info("Playing AI moves...");
                     updatedGame = movesProvider.playAIMoves(updatedGame.getAiType(), updatedGame, input, dbConn);
                 } else {
                     movesProvider.populateLastMoves(updatedGame, originalGame, input, dbConn);
                 }
+                LOG.info("MOVES SERVICE: Played AI moves");
 
                 if (updatedGame.getGameType() == GameType.TWO_PLAYER) {
                     ratingsProvider.updatePlayerRatings(updatedGame.getPlayer1Model(), updatedGame.getPlayer2Model(), updatedGame.getGameResult(), dbConn);
                 }
+                LOG.info("MOVES SERVICE: About to commit DB Connection...");
 
                 dbConn.commit();
+                LOG.info("MOVES SERVICE: Committed DB Connection. Updated model: {}", updatedGame);
                 return Response.ok(updatedGame).build();
             }
         } catch (Throwable e) {
