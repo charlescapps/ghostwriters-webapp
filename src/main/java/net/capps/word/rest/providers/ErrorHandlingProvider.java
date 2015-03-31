09737eaa2eeba9402c2f17661687af79b81391bf
Charles Capps <charles.capps@jivesoftware.com>
2015-03-30 20:59:22 -0700

    Add provider to make error-handling better

---
+++ src/main/java/net/capps/word/rest/providers/ErrorHandlingProvider.java
@@ -0,0 +1,24 @@
+package net.capps.word.rest.providers;
+
+import net.capps.word.rest.models.ErrorModel;
+
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.ExceptionMapper;
+import javax.ws.rs.ext.Provider;
+
+@Provider
+ public class ErrorHandlingProvider implements ExceptionMapper<Exception>
+{
+
+    @Override
+    public Response toResponse(Exception exception)
+    {
+        return Response
+                .status(Response.Status.BAD_REQUEST)
+                .entity( new ErrorModel(exception.getMessage()))
+                .type( MediaType.APPLICATION_JSON_TYPE)
+                .build();
+    }
+
+}
