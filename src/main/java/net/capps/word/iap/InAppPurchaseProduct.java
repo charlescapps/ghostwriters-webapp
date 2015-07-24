c6c5231d4852ba9bd5b3970bd3316d99e06f28a9
Charles Capps <charles.capps@jivesoftware.com>
2015-07-23 20:44:49 -0700

    Increase tokens for in-app purchases

--- src/main/java/net/capps/word/iap/InAppPurchaseProduct.java
+++ src/main/java/net/capps/word/iap/InAppPurchaseProduct.java
@@ -4,9 +4,9 @@ package net.capps.word.iap;
  * Created by charlescapps on 5/17/15.
  */
 public enum InAppPurchaseProduct {
-    book_pack_1(10),
-    book_pack_2(25),
-    book_pack_3(60),
+    book_pack_1(100),
+    book_pack_2(225),
+    book_pack_3(500),
     infinite_books(0);
 
     private final int numTokens;
