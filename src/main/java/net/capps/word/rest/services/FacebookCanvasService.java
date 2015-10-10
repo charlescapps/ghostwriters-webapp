986ca587cf50773b195434a512d3f83c7e85c7c9
Charles Capps <charles.capps@jivesoftware.com>
2015-10-10 12:49:26 -0700

    Add facebook data to the path, not sure what sort of data facebook sends.

--- src/main/java/net/capps/word/rest/services/FacebookCanvasService.java
+++ src/main/java/net/capps/word/rest/services/FacebookCanvasService.java
@@ -1,7 +1,11 @@
 package net.capps.word.rest.services;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
@@ -15,11 +19,15 @@ import java.net.URI;
 @Path("facebook")
 @Produces(MediaType.TEXT_HTML)
 public class FacebookCanvasService {
+    private static final Logger LOG = LoggerFactory.getLogger(FacebookCanvasService.class);
+
     @Context
     private UriInfo uriInfo;
 
     @POST
-    public Response getFacebookCanvas() {
+    @Path("{fbData}")
+    public Response getFacebookCanvas(@PathParam("fbData") String fbData) {
+        LOG.info("fbData={}", fbData);
         URI uri = uriInfo.getBaseUriBuilder()
                 .replacePath("/resources/html/facebook_canvas.html")
                 .build();
